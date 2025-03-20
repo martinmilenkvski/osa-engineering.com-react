89cc1b82012bde2dd95b5e787bec4667d68e98fd
martinmilenkvski <51443462+martinmilenkvski@users.noreply.github.com>
2025-03-20 20:57:10 +0100

    Structure setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Services />
+      <Gallery />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
