f9f34ca8a59274f55e07ee4b99bbeebb8d8dd150
martinmilenkvski <51443462+martinmilenkvski@users.noreply.github.com>
2025-03-22 16:14:34 +0100

    Hr + title for services

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Nav = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-transparent px-8 py-6">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 px-8 py-6 transition-colors duration-300 ${
+        scrolled ? "bg-black" : "bg-transparent"
+      }`}
+    >
       <div className="flex justify-between items-center">
         <img src="" alt="logo" className="h-12" />
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Services.css";
 
 const Services = () => {
+  const hrRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (hrRef.current) {
+        // Calculate scroll percentage
+        const scrollPercentage =
+          (window.scrollY /
+            (document.documentElement.scrollHeight - window.innerHeight)) *
+          400;
+        hrRef.current.style.width = `${scrollPercentage}%`;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div class="container">
-      <div class="card">Card 1</div>
-      <div class="card">Card 2</div>
-      <div class="card">Card 3</div>
+    <div>
+      <hr ref={hrRef} className="growing-hr mt-16" />
+      <p>OUR SERVICES</p>
+
+      <h2 className="text-8xl mb-12">
+        Our Range of Mechanical Engineering Services
+      </h2>
+
+      <div class="container">
+        <div class="card">Card 1</div>
+        <div class="card">Card 2</div>
+        <div class="card">Card 3</div>
+      </div>
     </div>
   );
 };
